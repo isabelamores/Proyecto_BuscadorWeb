be3b47e4ec3947df25e6e7b4c1c21416b8d0a9e4
isabelamores <101527075+isabelamores@users.noreply.github.com>
2024-02-08 12:09:11 -0600

    Add files via upload

--- proyectoweb/index.php
+++ proyectoweb/index.php
@@ -11,7 +11,44 @@
     <?php
     include 'menu.html';
     ?>
-   
+    <br><p></p>
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel" style="width: 100%; max-height: 400px; overflow: hidden;">
+  <div class="carousel-inner" style="height: 100%;">
+    <div class="carousel-item active">
+      <img src="images/imagen1.jpg" class="d-block w-100 h-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/imagen2.jpg" class="d-block w-100 h-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/imagen3.jpg" class="d-block w-100 h-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div><br><br>
+<div class="container">
+  <h1>Cafetería Kitty: Donde el Café y la Ternura se Mezclan</h1>
+  <p style="text-align: justify;"> Ubicada en un rincón encantador de la ciudad, Cafetería Kitty es mucho más que una simple experiencia de café; es un refugio acogedor donde los amantes de los felinos y del buen café se encuentran para disfrutar de momentos especiales. Esta cafetería única combina la pasión por el café de alta calidad con un toque de ternura felina, creando un ambiente acogedor y relajado.</p>
+  <h3> Catálogo de Productos:</h3>
+  <p style="text-align: justify;">
+  Cafetería Kitty ofrece una selección irresistible de bebidas que deleitan tanto el paladar como el corazón. Desde el clásico Espresso con su intensidad característica hasta el dulce y reconfortante Caramel Macchiato, cada taza está preparada con gran atención y dedicación. Los baristas expertos de Cafetería Kitty seleccionan cuidadosamente granos de café de alta calidad para garantizar una experiencia de sabor excepcional.
+  Además de las opciones de café, la cafetería presenta bebidas especiales, como el "Gatito Mocha" con un toque de chocolate y crema batida, o el "Latte de Fresa y Crema", una deliciosa mezcla que combina la frescura de las fresas con la suavidad de la crema.
+  </p>
+  <br>
+  <h3>Recetas Compartidas:</h3>
+  <p style="text-align: justify;">
+  Cafetería Kitty va más allá de simplemente servir café; también comparte el amor por los gatos a través de eventos y recetas temáticas. Los clientes pueden participar en talleres de arte latte donde aprenden a crear adorables diseños de gatitos en la superficie de sus bebidas. Las recetas de postres también se inspiran en el mundo felino, con opciones como "Galletas con Forma de Huella de Gato" y "Pastelito de Tuna y Crema".
+  </p>
+  <br>
+  <img src="images/cafe.jpg" style="display: block; margin: 0 auto;">
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="script.js"></script>
